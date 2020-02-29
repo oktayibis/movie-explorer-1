cef977ed8362ca6452e5da3b313b941816208744
Oktay Ibis <oktayibis@gmail.com>
2020-02-29 16:49:30 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import './App.css';
-import movies from './data';
-import MovieList from './MovieList';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import movies from "./data";
+import MovieList from "./MovieList";
+import Container from "react-bootstrap/Container";
+import { Row, Col, Navbar } from "react-bootstrap";
 
 export default class App extends React.Component {
   render() {
-    console.log(movies)
-    const renderMovie = (movieList) => {
+    console.log(movies);
+    const renderMovie = movieList => {
       return movieList.results.map(m => {
         return (
-        <div >
-          <MovieList key={m.id} title={m.title} overview ={m.overview} backdrop_path={m.backdrop_path} />
-        </div>
+          <Col sm={6}>
+            <MovieList
+              key={m.id}
+              title={m.title}
+              overview={m.overview}
+              backdrop_path={m.backdrop_path}
+              
+            />
+          </Col>
         );
-      })
-    }
+      });
+    };
     return (
-      <div className="Card">
-        <header className="App-header">
-        {renderMovie(movies)}
-        </header>
-      </div>
-
-     
+      <Container>
+      <Navbar expand="lg" variant="dark" bg="dark">
+        <Navbar.Brand href="#">The Movie Database</Navbar.Brand>
+      </Navbar>
+  
+   
+        <Row>{renderMovie(movies)}</Row>
+      </Container>
     );
   }
 }
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import { Button} from 'react-bootstrap';
+
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+
+  
 
 export default class MovieList extends Component {
     render() {
         
         return (
             
-            <div>
-            <img className='img-right' src={BACKDROP_BASE_URL+this.props.backdrop_path}></img>
-                <h3 className='text-left'>{this.props.title}</h3>
-                <p className='text-left'>{this.props.overview}</p>
-                <p>Add List </p>
-            </div>
+    
+             <div>
+                 <Image width='100%' src={BACKDROP_BASE_URL+this.props.backdrop_path} rounded />
+                <h3>{this.props.title}</h3>
+                <p >{this.props.overview} <Button variant="link">More</Button></p>            
+               
+                </div>
+      
+
         )
     }
 }
+/**
+ *  <Image src={BACKDROP_BASE_URL+this.props.backdrop_path} rounded />
+                <h3 className='text-left'>{this.props.title}</h3>
+                <p className='text-left'>{this.props.overview}</p>
+ */
